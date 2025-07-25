f635228b6ef3a2002d938e4838aa82265b590ac7
AsaiToshiya <to.asai.60@gmail.com>
2025-07-25 22:54:39 +0900

    Update getKindDescription.js

--- recent-activities/getKindDescription.js
+++ recent-activities/getKindDescription.js
@@ -38,6 +38,8 @@ export const getKindDescription = (kind) => {
     1063: "File Metadata",
     1081: "Personal Log",
     1111: "Comment",
+    1222: "Voice Message",
+    1244: "Voice Message Comment",
     1311: "Live Chat Message",
     1337: "Code Snippet",
     1617: "Patches",
